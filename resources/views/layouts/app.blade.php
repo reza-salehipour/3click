67de1e57fd907b25aeb730d990bae58704a892c3
rezas <rez.salehipour@gmail.com>
2023-07-04 09:46:55 +0100

    adding footer and complete some small bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,11 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{--    @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
 
     <link rel="stylesheet" href="{{asset('assets/css/font-awesome-pro-6.4.0.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap-5.3.min.css')}}">
 
 </head>
 <body style="background: #f5f7f9; overflow-x: hidden">
@@ -102,10 +103,212 @@
         </div>
     </header>
 </div>
-<div class="main vh-100 mx-3 overflow-x-hidden">
+<div class="main mx-3 overflow-x-hidden">
     @yield('content')
 </div>
+<footer class="bg-white">
+    <div class="container my-3 py-3">
+        <div class="row">
+            <div class="col-6 col-lg-4">
+                <p>About Us</p>
+                <p>London Flights</p>
+                <p>Manchester Flights</p>
+            </div>
+            <div class="col-6 col-lg-4">
+                <p>About Us</p>
+                <p>London Hotels</p>
+                <p>Manchester Hotels</p>
+            </div>
+            <div class="col-12 col-lg-4">
+                <p style="font-size: 14px;">
+                    Address: 60 Lancaster Street, Birmingham, England
+                </p>
+                <p>
+                    Social Media:
+                </p>
+                <div class="row justify-content-between">
+                    <div class="col-4">
+                        <i class="fa-brands fa-instagram fa-xl" style="color: #FD4175;"></i>
+                    </div>
+                    <div class="col-4">
+                        <i class="fa-brands fa-telegram fa-xl" style="color: #FD4175;"></i>
+                    </div>
+                    <div class="col-4">
+                        <i class="fa-brands fa-whatsapp fa-xl" style="color: #FD4175;"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</footer>
 <script src={{asset('assets/js/font-awesome-pro-6.4.0.min.js')}}></script>
+<script src={{asset('assets/js/bootstrap-5.3.min.js')}}></script>
+<script>
+    const form = document.querySelector(".form");
+    const dropdowns = document.querySelectorAll(".dropdown");
+
+    // Check if Dropdowns are Exist
+    // Loop Dropdowns and Create Custom Dropdown for each Select Element
+    if (dropdowns.length > 0) {
+        console.log('reza')
+        dropdowns.forEach((dropdown) => {
+            createCustomDropdown(dropdown);
+        });
+    }
+
+    // Check if Form Element Exist on Page
+    if (form !== null) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+        });
+    }
+
+    // Create Custom Dropdown
+    function createCustomDropdown(dropdown) {
+        // Get All Select Options
+        // And Convert them from NodeList to Array
+        const options = dropdown.querySelectorAll("option");
+        const optionsArr = Array.prototype.slice.call(options);
+
+        // Create Custom Dropdown Element and Add Class Dropdown
+        const customDropdown = document.createElement("div");
+        customDropdown.classList.add("dropdown");
+        dropdown.insertAdjacentElement("afterend", customDropdown);
+
+        // Create Element for Selected Option
+        const selected = document.createElement("div");
+        selected.classList.add("dropdown-select");
+        selected.textContent = optionsArr[0].textContent;
+        customDropdown.appendChild(selected);
+
+        // Create Element for Dropdown Menu
+        // Add Class and Append it to Custom Dropdown
+        const menu = document.createElement("div");
+        menu.classList.add("dropdown-menu");
+        customDropdown.appendChild(menu);
+        selected.addEventListener("click", toggleDropdown.bind(menu));
+
+        // Create Search Input Element
+        const search = document.createElement("input");
+        search.placeholder = "Search...";
+        search.type = "text";
+        search.classList.add("dropdown-menu-search");
+        menu.appendChild(search);
+
+        // Create Wrapper Element for Menu Items
+        // Add Class and Append to Menu Element
+        const menuInnerWrapper = document.createElement("div");
+        menuInnerWrapper.classList.add("dropdown-menu-inner");
+        menu.appendChild(menuInnerWrapper);
+
+        // Loop All Options and Create Custom Option for Each Option
+        // And Append it to Inner Wrapper Element
+        optionsArr.forEach((option) => {
+            const item = document.createElement("div");
+            item.classList.add("dropdown-menu-item");
+            item.dataset.value = option.value;
+            item.textContent = option.textContent;
+            menuInnerWrapper.appendChild(item);
+
+            item.addEventListener(
+                "click",
+                setSelected.bind(item, selected, dropdown, menu)
+            );
+        });
+
+        // Add Selected Class to First Custom Select Option
+        menuInnerWrapper.querySelector("div").classList.add("selected");
+
+        // Add Input Event to Search Input Element to Filter Items
+        // Add Click Event to Element to Close Custom Dropdown if Clicked Outside
+        // Hide the Original Dropdown(Select)
+        search.addEventListener("input", filterItems.bind(search, optionsArr, menu));
+        document.addEventListener(
+            "click",
+            closeIfClickedOutside.bind(customDropdown, menu)
+        );
+        dropdown.style.display = "none";
+    }
+
+    // Toggle for Display and Hide Dropdown
+    function toggleDropdown() {
+        if (this.offsetParent !== null) {
+            this.style.display = "none";
+        } else {
+            this.style.display = "block";
+            this.querySelector("input").focus();
+        }
+    }
+
+    // Set Selected Option
+    function setSelected(selected, dropdown, menu) {
+        // Get Value and Label from Clicked Custom Option
+        const value = this.dataset.value;
+        const label = this.textContent;
+
+        // Change the Text on Selected Element
+        // Change the Value on Select Field
+        selected.textContent = label;
+        dropdown.value = value;
+
+        // Close the Menu
+        // Reset Search Input Value
+        // Remove Selected Class from Previously Selected Option
+        // And Show All Div if they Were Filtered
+        // Add Selected Class to Clicked Option
+        menu.style.display = "none";
+        menu.querySelector("input").value = "";
+        menu.querySelectorAll("div").forEach((div) => {
+            if (div.classList.contains("is-select")) {
+                div.classList.remove("is-select");
+            }
+            if (div.offsetParent === null) {
+                div.style.display = "block";
+            }
+        });
+        this.classList.add("is-select");
+    }
+
+    // Filter the Items
+    function filterItems(itemsArr, menu) {
+        // Get All Custom Select Options
+        // Get Value of Search Input
+        // Get Filtered Items
+        // Get the Indexes of Filtered Items
+        const customOptions = menu.querySelectorAll(".dropdown-menu-inner div");
+        const value = this.value.toLowerCase();
+        const filteredItems = itemsArr.filter((item) =>
+            item.value.toLowerCase().includes(value)
+        );
+        const indexesArr = filteredItems.map((item) => itemsArr.indexOf(item));
+
+        // Check if Option is not Inside Indexes Array
+        // And Hide it and if it is Inside Indexes Array and it is Hidden Show it
+        itemsArr.forEach((option) => {
+            if (!indexesArr.includes(itemsArr.indexOf(option))) {
+                customOptions[itemsArr.indexOf(option)].style.display = "none";
+            } else {
+                if (customOptions[itemsArr.indexOf(option)].offsetParent === null) {
+                    customOptions[itemsArr.indexOf(option)].style.display = "block";
+                }
+            }
+        });
+    }
+
+    // Close Dropdown if Clicked Outside Dropdown Element
+    function closeIfClickedOutside(menu, e) {
+        if (
+            e.target.closest(".dropdown") === null &&
+            e.target !== this &&
+            menu.offsetParent !== null
+        ) {
+            menu.style.display = "none";
+        }
+    }
+
+</script>
+
 
 </body>
 </html>
